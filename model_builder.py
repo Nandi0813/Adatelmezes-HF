a102df221764b95ac54d47423565ebba2f26413d
Dukát Nándor <dukatnandor@gmail.com>
2024-11-25 10:39:59 +0100

    back to 2d

--- model_builder.py
+++ model_builder.py
@@ -23,27 +23,3 @@ def build_conv_pool_model(input_size, length):
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
     return model
-
-def build_conv_pool_3d_model(input_size, length):
-    model = models.Sequential()
-    model.add(layers.Conv3D(32, (3, 3, 3), activation='relu', input_shape=input_size, padding="valid"))
-    model.add(layers.BatchNormalization())
-    model.add(layers.MaxPooling3D((1, 2, 2)))
-
-    model.add(layers.Conv3D(64, (2, 2, 2), activation='relu', padding="valid"))
-    model.add(layers.BatchNormalization())
-    model.add(layers.MaxPooling3D((1, 2, 2)))
-
-    model.add(layers.Conv3D(128, (2, 2, 2), activation='relu', padding="valid"))
-    model.add(layers.BatchNormalization())
-    model.add(layers.MaxPooling3D((1, 2, 2)))
-
-    model.add(layers.Flatten())
-    model.add(layers.Dense(256, activation='relu'))
-    model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(length, activation='softmax'))
-
-    model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
-    return model
